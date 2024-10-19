14e7423574674b30256b0dfdda698b5c1838aea1
Nanta Es <ombakrinai@gmail.com>
2024-10-19 10:42:36 +0700

    add 'add button' to asset view

--- backend/views/asset/view.php
+++ backend/views/asset/view.php
@@ -90,7 +90,7 @@ $deleteAsset = Html::a('<i class="fa fa-trash"></i> Delete File', ['asset/delete
                 'hover' => true,
                 'mode' => Yii::$app->request->get('edit') == 't' ? DetailView::MODE_EDIT : DetailView::MODE_VIEW,
                 'panel' => [
-                    'heading' => $this->title,
+                    'heading' => $this->title.$create,
                     'type' => DetailView::TYPE_DEFAULT,
                 ],
                 'attributes' => [
