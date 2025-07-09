983447826063d8f5b6ae45ada71561f119ec73e2
esnanta <ombakrinai@gmail.com>
2025-07-09 15:58:39 +0700

    update

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,16 +1,20 @@
 <?php
 
+use yii\db\Connection;
+use yii\debug\Module;
+use yii\symfonymailer\Mailer;
+
 $config = [
     'components' => [
         'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2-news-local',
+            'class' => Connection::class,
+            'dsn' => 'mysql:host=localhost;dbname=yii2_news',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
         ],
         'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
+            'class' => Mailer::class,
             'viewPath' => '@common/mail',
             // send all mails to a file by default.
             'useFileTransport' => true,
@@ -40,41 +44,38 @@ $config = [
             // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
         ],
     ],
-    
 ];
-
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
-        'class' => \yii\debug\Module::class,
+        'class' => Module::class,
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii']['class'] = 'yii\gii\Module';
     $config['modules']['gii']['generators'] = [
+        'kartikgii-crud' => [ // generator name
+            'class' => 'common\templates\kartikgii\crud\Generator',
+            'templates' => [
+                'kartikgii' => '@common/templates/kartikgii/crud/default',
+            ],
+        ],
 
-            'kartikgii-crud' => [ // generator name
-                'class' => 'common\templates\kartikgii\crud\Generator',
-                'templates' => [
-                    'kartikgii' => '@common/templates/kartikgii/crud/default',
-                ]
+        'mootensai-crud' => [ // generator name
+            'class' => 'common\templates\mootensai\crud\Generator',
+            'templates' => [
+                'mootensai-crud' => '@common/templates/mootensai/crud/default',
             ],
-            
-            'mootensai-crud' => [ // generator name
-                'class' => 'common\templates\mootensai\crud\Generator',
-                'templates' => [
-                    'mootensai-crud' => '@common/templates/mootensai/crud/default',
-                ]
+        ],
+
+        'mootensai-model' => [ // generator name
+            'class' => 'common\templates\mootensai\model\Generator',
+            'templates' => [
+                'mootensai-model' => '@common/templates/mootensai/model/default',
             ],
-        
-            'mootensai-model' => [ // generator name
-                'class' => 'common\templates\mootensai\model\Generator',
-                'templates' => [
-                    'mootensai-model' => '@common/templates/mootensai/model/default',
-                ]
-            ],
+        ],
     ];
 }
 
