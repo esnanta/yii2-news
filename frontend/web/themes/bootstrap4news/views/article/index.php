21665e54a2bf18baf2e66253e1d6196ecb76c715
esnanta <ombakrinai@gmail.com>
2026-04-05 13:01:57 +0700

    add theme

---
+++ frontend/web/themes/bootstrap4news/views/article/index.php
@@ -0,0 +1,64 @@
+<?php
+
+use common\helper\MetaHelper;
+use yii\widgets\ListView;
+
+
+$this->title = Yii::t('app', 'Articles');
+MetaHelper::setMetaTags();
+?>
+
+<?php //echo $this->render('_search', ['model' => $searchModel]); ?>
+
+<?=
+ListView::widget([
+    'dataProvider' => $dataProvider,
+    'summary' => '',
+    'options' => [
+        'class' => 'row', // Bootstrap 4 grid row
+    ],
+    'itemOptions' => [
+        'tag' => 'div',
+        'class' => 'col-sm-6 mb-4', // Bootstrap 4 columns
+    ],
+    'pager' => [
+        'prevPageLabel' => '<i class="fa fa-angle-left mr-2"></i> Previous', // Bootstrap 4 margin utility
+        'nextPageLabel' => 'Next <i class="fa fa-angle-right ml-2"></i>',
+        'maxButtonCount' => 10,
+
+        // Surrounding container with Bootstrap 4 navigation classes
+        'options' => [
+            'tag' => 'nav',
+            'class' => 'd-flex justify-content-center', // Bootstrap 4 flex utilities
+            'aria-label' => 'Page Navigation',
+        ],
+
+        // Wrapping the links in a ul with pagination class
+        'linkContainerOptions' => [
+            'tag' => 'ul',
+            'class' => 'pagination', // Bootstrap 4 pagination class
+        ],
+
+        // Each link gets a page-item class
+        'linkOptions' => ['class' => 'page-link'], // Bootstrap 4 page link class
+
+        'activePageCssClass' => 'page-item active',
+        'disabledPageCssClass' => 'page-link page-item disabled',
+
+        // Customzing CSS class for navigating link
+        'pageCssClass' => ['class' => 'page-item'],
+        'prevPageCssClass' => 'page-item',
+        'nextPageCssClass' => 'page-item',
+        'firstPageCssClass' => 'page-first',
+        'lastPageCssClass' => 'page-last',
+
+
+    ],
+    'itemView' => '_index_grid', // Your view file for individual items
+]);
+?>
+
+
+
+
+
---
+++ frontend/web/themes/bootstrap4news/views/site/index.php
@@ -0,0 +1,180 @@
+<?php
+
+use common\helper\ContentHelper;
+use common\helper\MetaHelper;
+use common\service\ArticleService;
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+
+$this->title = Yii::$app->name;
+
+MetaHelper::setMetaTags();
+
+$articleService = new ArticleService();
+$articlesSlider = $articleService->getLatestArticles(3);
+$articlesPromo = $articleService->getLatestArticlesByOffset(4,4);
+$articlesPopular = $articleService->getPopularArticles(4,5);
+$articlesLatest = $articleService->getLatestArticlesByOffset(4,8);
+$articlesPinned = $articleService->getPinnedArticles(4);
+$articlesRandom = $articleService->getRandomArticles(9);
+?>
+
+
+<!-- Top News Start-->
+<div class="top-news">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 tn-left">
+                <div class="row tn-slider">
+                    <?php foreach ($articlesSlider as $i => $article) { ?>
+
+                        <div class="col-md-6">
+                            <div class="tn-img">
+                                <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'450','height'=>'350']);?>
+                                <div class="tn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+            <div class="col-md-6 tn-right">
+                <div class="row">
+                    <?php foreach ($articlesPromo as $i => $article) { ?>
+                        <div class="col-md-6">
+                            <div class="tn-img">
+                                <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'350px','height'=>'175px']);?>
+                                <div class="tn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Top News End-->
+
+
+
+<!-- Tab News Start-->
+<div class="tab-news">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="nav nav-pills nav-justified">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#popular">
+                            <?= Yii::t('app', 'Popular');?>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#latest">
+                            <?= Yii::t('app', 'Latest');?>
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+
+                    <div id="popular" class="container tab-pane active">
+                        <?php foreach ($articlesPopular as $i => $article) { ?>
+                            <div class="tn-news">
+                                <div class="tn-img">
+                                    <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'150px','height'=>'95px']);?>
+                                </div>
+                                <div class="tn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div id="latest" class="container tab-pane fade">
+                        <?php foreach ($articlesLatest as $i => $article) { ?>
+                            <div class="tn-news">
+                                <div class="tn-img">
+                                    <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'150px','height'=>'95px']);?>
+                                </div>
+                                <div class="tn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <ul class="nav nav-pills nav-justified">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#m-viewed">
+                            <?= Yii::t('app', 'Pinned');?>
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+                    <div id="m-viewed" class="container tab-pane active">
+                        <?php foreach ($articlesPinned as $i => $article) { ?>
+                            <div class="tn-news">
+                                <div class="tn-img">
+                                    <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'150px','height'=>'95px']);?>
+                                </div>
+                                <div class="tn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Tab News Start-->
+
+
+<!-- Main News Start-->
+<div class="main-news">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="row">
+
+                    <?php foreach ($articlesRandom as $i => $article) { ?>
+                        <div class="col-md-4">
+                            <div class="mn-img">
+                                <?= Html::img(ContentHelper::getCover($article->content,$article->cover),['width'=>'255px','height'=>'160px']);?>
+                                <div class="mn-title">
+                                    <?= Html::a($article->title, $article->getUrl()) ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="mn-list">
+                    <h2><?= Yii::t('app', 'Read More');?></h2>
+                    <ul>
+                        <?php foreach ($articlesLatest as $i => $article) { ?>
+                            <li>
+                                <?= Html::a($article->title, $article->getUrl()) ?>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Main News End-->
