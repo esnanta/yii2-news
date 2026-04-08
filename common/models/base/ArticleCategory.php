f11896c1ebcc9f2a5346dc4bc38cb248175487d7
esnanta <ombakrinai@gmail.com>
2026-04-08 21:20:50 +0700

    update

--- common/models/base/ArticleCategory.php
+++ common/models/base/ArticleCategory.php
@@ -2,35 +2,37 @@
 
 namespace common\models\base;
 
+use Yii;
 use common\base\BaseActiveRecord;
-use common\models\Article;
-use common\models\query\ArticleCategoryQuery;
-use mootensai\behaviors\UUIDBehavior;
-use mootensai\relation\RelationTrait;
-use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+use mootensai\behaviors\UUIDBehavior;
 use yii\db\ActiveQuery;
+use mootensai\relation\RelationTrait;
+use common\models\query\ArticleCategoryQuery;
+use common\models\Article;
 
 /**
  * This is the base model class for table "t_article_category".
  *
- * @property int               $id
- * @property string            $slug
- * @property string            $title
- * @property string            $body
- * @property int               $parent_id
- * @property int               $status
- * @property string            $created_at
- * @property string            $updated_at
- * @property int               $created_by
- * @property int               $updated_by
- * @property int               $is_deleted
- * @property string            $deleted_at
- * @property int               $deleted_by
- * @property int               $verlock
- * @property string            $uuid
- * @property Article[]         $articles
- * @property ArticleCategory   $parent
+ * @property integer $id
+ * @property string $slug
+ * @property string $title
+ * @property string $body
+ * @property integer $parent_id
+ * @property integer $status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property integer $created_by
+ * @property integer $updated_by
+ * @property integer $is_deleted
+ * @property string $deleted_at
+ * @property integer $deleted_by
+ * @property integer $verlock
+ * @property string $uuid
+ *
+ * @property Article[] $articles
+ * @property ArticleCategory $parent
  * @property ArticleCategory[] $articleCategories
  */
 class ArticleCategory extends BaseActiveRecord
@@ -40,8 +42,7 @@ class ArticleCategory extends BaseActiveRecord
     private $_rt_softdelete;
     private $_rt_softrestore;
 
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->_rt_softdelete = [
             'deleted_by' => \Yii::$app->user->id,
@@ -54,19 +55,21 @@ class ArticleCategory extends BaseActiveRecord
     }
 
     /**
-     * This function helps \mootensai\relation\RelationTrait runs faster.
-     *
-     * @return array relation names of this model
-     */
+    * This function helps \mootensai\relation\RelationTrait runs faster
+    * @return array relation names of this model
+    */
     public function relationNames(): array
     {
         return [
             'articles',
             'parent',
-            'articleCategories',
+            'articleCategories'
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function rules(): array
     {
         return [
@@ -79,56 +82,73 @@ class ArticleCategory extends BaseActiveRecord
             [['uuid'], 'string', 'max' => 36],
             [['slug'], 'unique'],
             [['verlock'], 'default', 'value' => '0'],
-            [['verlock'], 'mootensai\components\OptimisticLockValidator'],
+            [['verlock'], 'mootensai\components\OptimisticLockValidator']
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     public static function tableName(): string
     {
         return 't_article_category';
     }
 
     /**
+     *
      * @return string
-     *                overwrite function optimisticLock
-     *                return string name of field are used to stored optimistic lock
+     * overwrite function optimisticLock
+     * return string name of field are used to stored optimistic lock
+     *
      */
-    public function optimisticLock(): string
-    {
+    public function optimisticLock(): string {
         return 'verlock';
     }
 
+    /**
+     * @inheritdoc
+     */
     public function attributeLabels(): array
     {
         return [
-            'id' => \Yii::t('common', 'ID'),
-            'slug' => \Yii::t('common', 'Slug'),
-            'title' => \Yii::t('common', 'Title'),
-            'body' => \Yii::t('common', 'Body'),
-            'parent_id' => \Yii::t('common', 'Parent ID'),
-            'status' => \Yii::t('common', 'Status'),
-            'is_deleted' => \Yii::t('common', 'Is Deleted'),
-            'verlock' => \Yii::t('common', 'Verlock'),
-            'uuid' => \Yii::t('common', 'Uuid'),
+            'id' => Yii::t('common', 'ID'),
+            'slug' => Yii::t('common', 'Slug'),
+            'title' => Yii::t('common', 'Title'),
+            'body' => Yii::t('common', 'Body'),
+            'parent_id' => Yii::t('common', 'Parent ID'),
+            'status' => Yii::t('common', 'Status'),
+            'is_deleted' => Yii::t('common', 'Is Deleted'),
+            'verlock' => Yii::t('common', 'Verlock'),
+            'uuid' => Yii::t('common', 'Uuid'),
         ];
     }
-
+    
+    /**
+     * @return ActiveQuery
+     */
     public function getArticles(): ActiveQuery
     {
         return $this->hasMany(Article::class, ['category_id' => 'id']);
     }
-
+        
+    /**
+     * @return ActiveQuery
+     */
     public function getParent(): ActiveQuery
     {
         return $this->hasOne(ArticleCategory::class, ['id' => 'parent_id']);
     }
-
+        
+    /**
+     * @return ActiveQuery
+     */
     public function getArticleCategories(): ActiveQuery
     {
         return $this->hasMany(ArticleCategory::class, ['parent_id' => 'id']);
     }
-
+    
     /**
+     * @inheritdoc
      * @return array mixed
      */
     public function behaviors(): array
@@ -175,12 +195,11 @@ class ArticleCategory extends BaseActiveRecord
      */
 
     /**
-     * @return ArticleCategoryQuery the active query used by this AR class
+     * @inheritdoc
+     * @return ArticleCategoryQuery the active query used by this AR class.
      */
-    public static function find(): ArticleCategoryQuery
-    {
+    public static function find(): ArticleCategoryQuery    {
         $query = new ArticleCategoryQuery(get_called_class());
-
         return $query->where(['t_article_category.deleted_by' => 0]);
     }
 }
