cf024230e4c4d6a24fd8b616765a4a4f9031b6a9
esnanta <ombakrinai@gmail.com>
2026-04-08 16:41:52 +0700

    update

--- common/migrations/db/m260405_100000_widget_image.php
+++ common/migrations/db/m260405_100000_widget_image.php
@@ -24,6 +24,73 @@ class m260405_100000_widget_image extends Migration
         ]);
 
         $this->createIndex('idx-widget_image-key', '{{%widget_image}}', 'key');
+
+        $now = time();
+        $frontendUrl = \Yii::getAlias('@frontendUrl');
+        $themeImageBaseUrl = $frontendUrl . '/themes/bootstrap4news/assets/img';
+
+        // Default placeholders for common page image slots.
+        $this->batchInsert('{{%widget_image}}', [
+            'key',
+            'title',
+            'base_url',
+            'path',
+            'mime_type',
+            'size',
+            'link_url',
+            'alt_text',
+            'created_at',
+            'updated_at',
+        ], [
+            [
+                'logo_top',
+                'Logo Header',
+                $themeImageBaseUrl,
+                'logo.png',
+                'image/png',
+                null,
+                $frontendUrl,
+                'Logo website di bagian atas',
+                $now,
+                $now,
+            ],
+            [
+                'logo_bottom',
+                'Logo Footer',
+                $themeImageBaseUrl,
+                'logo.png',
+                'image/png',
+                null,
+                $frontendUrl,
+                'Logo website di bagian bawah',
+                $now,
+                $now,
+            ],
+            [
+                'hero_horizontal_top_center',
+                'Banner Horizontal Atas Tengah',
+                $themeImageBaseUrl,
+                'ads-1.jpg',
+                'image/jpeg',
+                null,
+                $frontendUrl,
+                'Banner horizontal pada bagian atas tengah halaman',
+                $now,
+                $now,
+            ],
+            [
+                'hero_horizontal_top_center_alt',
+                'Banner Horizontal Atas Tengah Alternatif',
+                $themeImageBaseUrl,
+                'ads-2.jpg',
+                'image/jpeg',
+                null,
+                $frontendUrl,
+                'Banner horizontal alternatif untuk area atas tengah',
+                $now,
+                $now,
+            ],
+        ]);
     }
 
     /**
