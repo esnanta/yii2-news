a72a3aa2fd0d822a68daa9db957293703a0fabb6
esnanta <ombakrinai@gmail.com>
2026-04-11 00:39:06 +0700

    add article search model

---
+++ common/models/search/ArticleSearch.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace common\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Article;
+
+/**
+ * ArticleSearch represents the model behind the search form about `common\models\Article`.
+ */
+class ArticleSearch extends Article
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return [
+            [['id', 'author_id', 'category_id', 'status', 'created_by', 'updated_by', 'is_deleted', 'deleted_by', 'verlock'], 'integer'],
+            [['slug', 'title', 'body', 'view', 'thumbnail_base_url', 'thumbnail_path', 'created_at', 'updated_at', 'published_at', 'deleted_at', 'uuid'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios(): array
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search(array $params): ActiveDataProvider
+    {
+        $query = Article::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'author_id' => $this->author_id,
+            'category_id' => $this->category_id,
+            'status' => $this->status,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'published_at' => $this->published_at,
+            'is_deleted' => $this->is_deleted,
+            'deleted_at' => $this->deleted_at,
+            'deleted_by' => $this->deleted_by,
+            'verlock' => $this->verlock,
+        ]);
+
+        $query->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'body', $this->body])
+            ->andFilterWhere(['like', 'view', $this->view])
+            ->andFilterWhere(['like', 'thumbnail_base_url', $this->thumbnail_base_url])
+            ->andFilterWhere(['like', 'thumbnail_path', $this->thumbnail_path])
+            ->andFilterWhere(['like', 'uuid', $this->uuid]);
+
+        return $dataProvider;
+    }
+}
