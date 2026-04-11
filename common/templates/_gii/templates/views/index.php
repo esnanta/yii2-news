8178eb24b3c546ae6153ad6aa6fabd21fd647980
esnanta <ombakrinai@gmail.com>
2026-04-11 12:04:46 +0700

    update layout

--- common/templates/_gii/templates/views/index.php
+++ common/templates/_gii/templates/views/index.php
@@ -11,10 +11,53 @@ use yii\helpers\StringHelper;
 $urlParams = $generator->generateUrlParams();
 $nameAttribute = $generator->getNameAttribute();
 
+$buildGridColumn = static function (string $name, string $format = 'text'): string {
+    $normalized = strtolower($name);
+    $isIdColumn = $normalized === 'id';
+    $isForeignKey = substr($normalized, -3) === '_id';
+    $isDateColumn = strpos($normalized, '_at') !== false || strpos($normalized, 'date') !== false;
+    $isWrapColumn = preg_match('/(title|name|description|username|url|slug|address)/', $normalized) === 1
+        || in_array($format, ['ntext', 'text', 'url'], true);
+
+    $width = null;
+    if ($isIdColumn) {
+        $width = 8;
+    } elseif ($isForeignKey) {
+        $width = 12;
+    } elseif ($isDateColumn) {
+        $width = 12;
+    } elseif (in_array($normalized, ['status', 'sequence', 'is_active', 'is_visible', 'is_primary'], true)) {
+        $width = 10;
+    } elseif ($isWrapColumn) {
+        $width = 25;
+    }
+
+    $contentStyle = ($isDateColumn || $isIdColumn || $isForeignKey) && !$isWrapColumn
+        ? 'white-space: nowrap;'
+        : 'white-space: normal; word-break: break-word;';
+
+    $result = "[\n";
+    $result .= "    'attribute' => '{$name}',\n";
+
+    if ($format !== 'text') {
+        $result .= "    'format' => '{$format}',\n";
+    }
+
+    if ($width !== null) {
+        $result .= "    'options' => ['style' => 'width: {$width}%'],\n";
+    }
+
+    $result .= "    'contentOptions' => ['style' => '{$contentStyle}'],\n";
+    $result .= '],';
+
+    return $result;
+};
+
 echo "<?php\n";
 ?>
 
 use yii\helpers\Html;
+use rmrevin\yii\fontawesome\FAS;
 use <?php echo $generator->indexWidgetType === 'grid' ? "yii\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
 
 /**
@@ -29,7 +72,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="<?php echo Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
     <div class="card">
         <div class="card-header">
-            <?php echo "<?php echo " ?>Html::a(<?php echo $generator->generateString('Create {modelClass}', ['modelClass' => Inflector::camel2words(StringHelper::basename($generator->modelClass))]) ?>, ['create'], ['class' => 'btn btn-success']) ?>
+            <?php echo "<?php echo " ?>Html::a(FAS::icon('user-plus').' '.<?php echo $generator->generateString('Add New {modelClass}', ['modelClass' => Inflector::camel2words(StringHelper::basename($generator->modelClass))]) ?>, ['create'], ['class' => 'btn btn-success']); ?>
         </div>
 
         <div class="card-body <?php echo $generator->indexWidgetType === 'grid'? 'p-0': '' ?>">
@@ -44,18 +87,23 @@ $this->params['breadcrumbs'][] = $this->title;
                     'class' => ['gridview', 'table-responsive'],
                 ],
                 'tableOptions' => [
-                    'class' => ['table', 'text-nowrap', 'table-striped', 'table-bordered', 'mb-0'],
+                    'class' => ['table', 'table-striped', 'table-bordered', 'mb-0', 'table-sm'],
+                    'style' => 'width: 100%; table-layout: fixed;',
                 ],
                 'dataProvider' => $dataProvider,
                 <?php echo !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n                'columns' => [\n" : "'columns' => [\n"; ?>
-                    ['class' => 'yii\grid\SerialColumn'],
+                    [
+                        'class' => 'yii\grid\SerialColumn',
+                        'options' => ['style' => 'width: 5%'],
+                        'contentOptions' => ['style' => 'white-space: nowrap;'],
+                    ],
 
                     <?php
                     $count = 0;
                     if (($tableSchema = $generator->getTableSchema()) === false) {
                         foreach ($generator->getColumnNames() as $name) {
                             if (++$count < 6) {
-                                echo "'" . $name . "',\n";
+                                echo $buildGridColumn($name) . "\n                    ";
                             } else {
                                 echo "// '" . $name . "',\n";
                             }
@@ -64,7 +112,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         foreach ($tableSchema->columns as $column) {
                             $format = $generator->generateColumnFormat($column);
                             if (++$count < 6) {
-                                echo "'" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n                    ";
+                                echo $buildGridColumn($column->name, $format) . "\n                    ";
                             } else {
                                 echo "// '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n                    ";
                             }
@@ -72,7 +120,11 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
         ?>
 
-                    ['class' => \common\widgets\ActionColumn::class],
+                    [
+                        'class' => \common\widgets\ActionColumn::class,
+                        'options' => ['style' => 'width: 5%'],
+                        'contentOptions' => ['style' => 'white-space: nowrap;'],
+                    ],
                 ],
             ]); ?>
     <?php else: ?>
