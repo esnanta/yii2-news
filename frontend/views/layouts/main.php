ed8ade615c2bb5baf4aca4b0f74440ba6cb1a1b0
esnanta <ombakrinai@gmail.com>
2026-04-15 16:35:57 +0700

    fix about url to use page/view

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,32 +1,35 @@
 <?php
 /**
- * @var yii\web\View $this
+ * @var View   $this
  * @var string $content
  */
 
-use yii\helpers\ArrayHelper;
+use common\widgets\DbText;
+use yii\bootstrap4\Alert;
 use yii\bootstrap4\Breadcrumbs;
+use yii\helpers\ArrayHelper;
+use yii\web\View;
 
-$this->beginContent('@frontend/views/layouts/base.php')
+$this->beginContent('@frontend/views/layouts/base.php');
 ?>
 <div class="container">
 
     <?php echo Breadcrumbs::widget([
         'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-    ]) ?>
+    ]); ?>
 
-    <?php if(Yii::$app->session->hasFlash('alert')):?>
-        <?php echo \yii\bootstrap4\Alert::widget([
-            'body'=>ArrayHelper::getValue(Yii::$app->session->getFlash('alert'), 'body'),
-            'options'=>ArrayHelper::getValue(Yii::$app->session->getFlash('alert'), 'options'),
-        ])?>
-    <?php endif; ?>
+    <?php if (Yii::$app->session->hasFlash('alert')) { ?>
+        <?php echo Alert::widget([
+            'body' => ArrayHelper::getValue(Yii::$app->session->getFlash('alert'), 'body'),
+            'options' => ArrayHelper::getValue(Yii::$app->session->getFlash('alert'), 'options'),
+        ]); ?>
+    <?php } ?>
 
     <!-- Example of your ads placing -->
-    <?php echo \common\widgets\DbText::widget([
-        'key' => 'ads-example'
-    ]) ?>
+    <?php echo DbText::widget([
+        'key' => 'ads-example',
+    ]); ?>
 
-    <?php echo $content ?>
+    <?php echo $content; ?>
 </div>
-<?php $this->endContent() ?>
+<?php $this->endContent(); ?>
