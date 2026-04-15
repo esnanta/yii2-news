ceb0cdfe27ae1582218f99eb6f8497d451865977
esnanta <ombakrinai@gmail.com>
2026-04-15 15:38:20 +0700

    fix article widget

--- common/widgets/bootstrap4news/RecentArticles.php
+++ common/widgets/bootstrap4news/RecentArticles.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace common\widgets\bootstrap4news;
 
-use Yii;
+use common\models\Article;
 use yii\base\Widget;
-use common\models\Article as Article;
 
 class RecentArticles extends Widget
 {
@@ -14,15 +14,20 @@ class RecentArticles extends Widget
     {
         parent::init();
 
-        if ($this->title === null) {
+        if (null === $this->title) {
             $this->title = 'title';
         }
     }
 
     public function run()
     {
-        $articleList = Article::find()->where(['publish_status' => Article::PUBLISH_STATUS_YES])->orderBy(['created_at' => SORT_DESC])->limit($this->maxData)->all();
-        return $this->render('_recent_blog', [
+        $articleList = Article::find()
+            ->where(['status' => Article::STATUS_PUBLISHED])
+            ->orderBy(['created_at' => SORT_DESC])
+            ->limit($this->maxData)->all()
+        ;
+
+        return $this->render('_recent_article', [
             'title' => $this->title,
             'articleList' => $articleList,
         ]);
