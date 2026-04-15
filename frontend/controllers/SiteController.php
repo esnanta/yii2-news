3d3b5e3b074eb0f66f94227955a57b422cf4c7eb
esnanta <ombakrinai@gmail.com>
2026-04-15 16:33:12 +0700

    update with cs-fixer

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -7,14 +7,13 @@ use common\sitemap\UrlsIterator;
 use frontend\models\ContactForm;
 use Sitemaped\Element\Urlset\Urlset;
 use Sitemaped\Sitemap;
-use Yii;
 use yii\filters\PageCache;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\Response;
 
 /**
- * Site controller
+ * Site controller.
  */
 class SiteController extends Controller
 {
@@ -28,27 +27,24 @@ class SiteController extends Controller
                 'class' => PageCache::class,
                 'only' => ['sitemap'],
                 'duration' => Time::SECONDS_IN_AN_HOUR,
-            ]
+            ],
         ];
     }
 
-    /**
-     * @inheritdoc
-     */
     public function actions()
     {
         return [
             'error' => [
-                'class' => 'yii\web\ErrorAction'
+                'class' => 'yii\web\ErrorAction',
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
             'set-locale' => [
                 'class' => 'common\actions\SetLocaleAction',
-                'locales' => array_keys(Yii::$app->params['availableLocales'])
-            ]
+                'locales' => array_keys(\Yii::$app->params['availableLocales']),
+            ],
         ];
     }
 
@@ -61,53 +57,56 @@ class SiteController extends Controller
     }
 
     /**
-     * @return string|Response
+     * @return Response|string
      */
     public function actionContact()
     {
         $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($model->contact(Yii::$app->params['adminEmail'])) {
-                Yii::$app->getSession()->setFlash('alert', [
-                    'body' => Yii::t('frontend', 'Thank you for contacting us. We will respond to you as soon as possible.'),
-                    'options' => ['class' => 'alert-success']
+        if ($model->load(\Yii::$app->request->post())) {
+            if ($model->contact(\Yii::$app->params['adminEmail'])) {
+                \Yii::$app->getSession()->setFlash('alert', [
+                    'body' => \Yii::t(
+                        'frontend',
+                        'Thank you for contacting us. We will respond to you as soon as possible.'
+                    ),
+                    'options' => ['class' => 'alert-success'],
                 ]);
+
                 return $this->refresh();
             }
 
-            Yii::$app->getSession()->setFlash('alert', [
+            \Yii::$app->getSession()->setFlash('alert', [
                 'body' => \Yii::t('frontend', 'There was an error sending email.'),
-                'options' => ['class' => 'alert-danger']
+                'options' => ['class' => 'alert-danger'],
             ]);
         }
 
         return $this->render('contact', [
-            'model' => $model
+            'model' => $model,
         ]);
     }
 
     /**
-     * @param string $format
-     * @param bool $gzip
-     * @return string
      * @throws BadRequestHttpException
      */
-    public function actionSitemap($format = Sitemap::FORMAT_XML, $gzip = false)
-    {
+    public function actionSitemap(
+        string $format = Sitemap::FORMAT_XML,
+        bool $gzip = false
+    ): string {
         $links = new UrlsIterator();
         $sitemap = new Sitemap(new Urlset($links));
 
-        Yii::$app->response->format = Response::FORMAT_RAW;
+        \Yii::$app->response->format = Response::FORMAT_RAW;
 
-        if ($gzip === true) {
-            Yii::$app->response->headers->add('Content-Encoding', 'gzip');
+        if (true === $gzip) {
+            \Yii::$app->response->headers->add('Content-Encoding', 'gzip');
         }
 
-        if ($format === Sitemap::FORMAT_XML) {
-            Yii::$app->response->headers->add('Content-Type', 'application/xml');
+        if (Sitemap::FORMAT_XML === $format) {
+            \Yii::$app->response->headers->add('Content-Type', 'application/xml');
             $content = $sitemap->toXmlString($gzip);
-        } else if ($format === Sitemap::FORMAT_TXT) {
-            Yii::$app->response->headers->add('Content-Type', 'text/plain');
+        } elseif (Sitemap::FORMAT_TXT === $format) {
+            \Yii::$app->response->headers->add('Content-Type', 'text/plain');
             $content = $sitemap->toTxtString($gzip);
         } else {
             throw new BadRequestHttpException('Unknown format');
@@ -115,7 +114,7 @@ class SiteController extends Controller
 
         $linksCount = $sitemap->getCount();
         if ($linksCount > 50000) {
-            Yii::warning(\sprintf('Sitemap links count is %d', $linksCount));
+            \Yii::warning(\sprintf('Sitemap links count is %d', $linksCount));
         }
 
         return $content;
