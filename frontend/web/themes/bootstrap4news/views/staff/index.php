f1002a1cf86891f5e9f7e6ceb2bf8c8ac76707fb
esnanta <ombakrinai@gmail.com>
2026-04-18 00:21:28 +0700

    update

--- frontend/web/themes/bootstrap4news/views/staff/index.php
+++ frontend/web/themes/bootstrap4news/views/staff/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use yii\helpers\Html;
 use yii\widgets\ListView;
 
 /**
@@ -8,30 +9,29 @@ use yii\widgets\ListView;
  * @var common\models\search\StaffSearch $searchModel
  */
 
-$this->title = 'Staff';
+$this->title = 'Our Team';
 $this->params['breadcrumbs'][] = $this->title;
-
-$col = 'col-md-4';
-$countData = $dataProvider->getTotalCount();
-
-if (2 == $countData) {
-    $col = 'col-md-4 offset-md-2';
-} elseif ($countData >= 3) {
-    $col = 'col-md-4';
-}
 ?>
+
+<div class="container py-5">
+    <div class="row text-center mb-5">
+        <div class="col-lg-8 mx-auto">
+            <h1 class="display-4"><?php echo Html::encode($this->title); ?></h1>
+            <p class="lead text-muted">Meet our dedicated team of professionals.</p>
+        </div>
+    </div>
 
     <?php echo ListView::widget([
         'dataProvider' => $dataProvider,
         'summary' => false,
-        'emptyText' => 'No staff found.',
+        'emptyText' => '<div class="col"><p class="text-center">No staff members found.</p></div>',
         'options' => [
             'tag' => 'div',
             'class' => 'row',
         ],
         'itemOptions' => [
             'tag' => 'div',
-            'class' => $col.' mb-4',
+            'class' => 'col-lg-4 col-md-6 mb-5',
         ],
         'pager' => [
             'prevPageLabel' => '<i class="fa fa-angle-left"></i> Previous',
@@ -49,3 +49,4 @@ if (2 == $countData) {
         ],
         'itemView' => '_index_feature',
     ]); ?>
+</div>
