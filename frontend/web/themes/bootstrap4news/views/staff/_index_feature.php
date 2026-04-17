48b8527874921ae2fcf00ed09dbec9b472c792d8
esnanta <ombakrinai@gmail.com>
2026-04-18 00:00:39 +0700

    update

--- frontend/web/themes/bootstrap4news/views/staff/_index_feature.php
+++ frontend/web/themes/bootstrap4news/views/staff/_index_feature.php
@@ -1,12 +1,12 @@
 <?php
 
-use common\models\StaffMedia;
+use common\models\Staff;
+use common\models\StaffSocialAccount;
 use yii\helpers\Html;
 
 /**
- * @var $model common\models\Staff
+ * @var Staff $model
  */
-
 $img = Html::img(str_replace('frontend', 'backend', $model->getAssetUrl()), [
     'class' => 'img-fluid rounded mb-3',
     'alt' => $model->title,
@@ -18,31 +18,31 @@ $linkClass = 'text-dark font-weight-bold';
 
 <div class="card h-100">
     <div class="card-body text-center">
-        <?= $img ?>
+        <?php echo $img; ?>
 
         <em class="d-block text-uppercase text-primary mb-2">
-            <?= $model->employment_id ? $model->employment->title : 'Not Set'; ?>
+            <?php echo $model->employment_id ? $model->employment->title : 'Not Set'; ?>
         </em>
 
         <h5 class="card-title">
-            <?= Html::a(Html::encode($model->title), $model->getUrl(), ['class' => $linkClass]) ?>
+            <?php echo Html::a(Html::encode($model->title), $model->getUrl(), ['class' => $linkClass]); ?>
         </h5>
 
-        <p class="card-text text-muted"><?= Html::encode($model->description) ?></p>
+        <p class="card-text text-muted"><?php echo Html::encode($model->description); ?></p>
     </div>
 
     <div class="card-footer bg-transparent border-top-0">
-        <?php $staffMedias = StaffMedia::find()->where(['staff_id' => $model->id])->all(); ?>
+        <?php $staffMedias = StaffSocialAccount::find()->where(['staff_id' => $model->id])->all(); ?>
         <ul class="list-inline text-center mb-0">
-            <?php foreach ($staffMedias as $staffMediaItem): ?>
+            <?php foreach ($staffMedias as $staffMediaItem) { ?>
                 <li class="list-inline-item">
-                    <?= Html::a('<i class="' . $staffMediaItem->title . '"></i>', $staffMediaItem->description, [
+                    <?php echo Html::a('<i class="'.$staffMediaItem->title.'"></i>', $staffMediaItem->description, [
                         'class' => 'text-muted',
                         'target' => '_blank',
-                        'rel' => 'noopener noreferrer'
-                    ]) ?>
+                        'rel' => 'noopener noreferrer',
+                    ]); ?>
                 </li>
-            <?php endforeach; ?>
+            <?php } ?>
         </ul>
     </div>
 </div>
