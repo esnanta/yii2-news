3b73d15dbcc4d0cc852a86d5dc336c4af4e9d0ce
esnanta <ombakrinai@gmail.com>
2026-04-18 10:56:24 +0700

    update with cs-fixer

--- frontend/web/themes/bootstrap4news/views/document/view.php
+++ frontend/web/themes/bootstrap4news/views/document/view.php
@@ -4,7 +4,7 @@ use common\models\Document;
 use rmrevin\yii\fontawesome\FAS;
 use yii\helpers\Html;
 
-/*
+/**
  * @var yii\web\View $this
  * @var common\models\Document $model
  */
@@ -19,17 +19,18 @@ $documentUrl = $model->getUrl();
 <div class="document-view">
     <div class="card">
         <div class="card-header">
-            <h1 class="card-title h2"><?= Html::encode($this->title) ?></h1>
+            <h1 class="card-title h2"><?php echo Html::encode($this->title); ?></h1>
         </div>
         <div class="card-body">
-            <?php if (!empty($model->description)) : ?>
-                <p class="card-text"><?= nl2br(Html::encode($model->description)) ?></p>
-            <?php endif; ?>
+            <?php if (!empty($model->description)) { ?>
+                <p class="card-text"><?php echo nl2br(Html::encode($model->description)); ?></p>
+            <?php } ?>
 
             <?php
             switch ($model->document_type) {
                 case Document::TYPE_IMAGE:
                     echo Html::img($documentUrl, ['class' => 'img-fluid', 'alt' => $model->title]);
+
                     break;
 
                 case Document::TYPE_PDF:
@@ -39,27 +40,38 @@ $documentUrl = $model->getUrl();
                         'height' => '800px',
                         'style' => 'border: none;',
                     ]);
+
                     break;
 
                 case Document::TYPE_OFFICE_DOCUMENT:
                     // Gunakan Microsoft Office Online Viewer untuk menyematkan dokumen office
-                    $officeViewerUrl = 'https://view.officeapps.live.com/op/embed.aspx?src=' . urlencode($documentUrl);
+                    $officeViewerUrl = 'https://view.officeapps.live.com/op/embed.aspx?src='.urlencode($documentUrl);
                     echo Html::tag('iframe', 'Your browser does not support iframes.', [
                         'src' => $officeViewerUrl,
                         'width' => '100%',
                         'height' => '800px',
                         'frameborder' => '0',
                     ]);
+
                     break;
 
                 default:
                     echo '<div class="alert alert-warning">Pratinjau tidak tersedia untuk tipe dokumen ini.</div>';
+
                     break;
             }
-            ?>
+?>
         </div>
         <div class="card-footer text-muted">
-            <?= Html::a(FAS::icon('download') . ' Download', $documentUrl, ['class' => 'btn btn-primary', 'target' => '_blank', 'rel' => 'noopener noreferrer']) ?>
+            <?php echo Html::a(
+                FAS::icon('download')
+                .' Download',
+                $documentUrl,
+                ['class' => 'btn btn-primary',
+                    'target' => '_blank',
+                    'rel' => 'noopener noreferrer',
+                ]
+            ); ?>
         </div>
     </div>
 </div>
