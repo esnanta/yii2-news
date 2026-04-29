abe6cfec698904a159c5be594c52240400044e50
esnanta <ombakrinai@gmail.com>
2026-04-29 18:49:19 +0700

    update

--- frontend/web/themes/bootstrap4news/views/article/view.php
+++ frontend/web/themes/bootstrap4news/views/article/view.php
@@ -238,9 +238,16 @@ MetaHelper::setMetaTags([
 
 
                     (function() {
-                        var gc = document.createElement('script'); gc.type = 'text/javascript'; gc.async = true;
-                        gc.onload = __semio__onload; gc.defer = true; gc.src = 'https://integration.graphcomment.com/gc_graphlogin.js?' + Date.now();
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(gc);
+                        var gc = document.createElement('script');
+                        gc.type = 'text/javascript';
+                        gc.async = true;
+                        gc.onload = __semio__onload;
+                        gc.defer = true;
+                        gc.src = 'https://integration.graphcomment.com/gc_graphlogin.js?' + Date.now();
+
+                        var target = document.getElementsByTagName('head')[0]
+                            || document.getElementsByTagName('body')[0];
+                        target.appendChild(gc);
                     })();
 
 
