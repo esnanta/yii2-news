3f417558a171bfb7ba23fb88632c783579b4af0c
esnanta <ombakrinai@gmail.com>
2026-05-24 09:57:37 +0700

    update rules method

--- common/models/OfficeSocialAccount.php
+++ common/models/OfficeSocialAccount.php
@@ -38,25 +38,22 @@ class OfficeSocialAccount extends BaseOfficeSocialAccount
 
     public function rules(): array
     {
-        return array_replace_recursive(
-            parent::rules(),
-            [
-                [['platform_id'], 'required'],
-                [['office_id', 'platform_id', 'is_primary', 'is_visible',
-                    'sequence', 'created_by', 'updated_by',
-                    'is_deleted', 'deleted_by', 'verlock'], 'integer'],
-                [['description'], 'string'],
-                [['created_at', 'updated_at', 'deleted_at'], 'safe'],
-                [['title'], 'string', 'max' => 100],
-                [['profile_url'], 'string', 'max' => 500],
-                [['uuid'], 'string', 'max' => 36],
-                [['office_id', 'platform_id'], 'unique',
-                    'targetAttribute' => ['office_id', 'platform_id'],
-                    'message' => 'The combination of Office ID and Platform ID has already been taken.'],
-                [['verlock'], 'default', 'value' => '0'],
-                [['verlock'], 'mootensai\components\OptimisticLockValidator'],
-            ]
-        );
+        return [
+            [['platform_id'], 'required'],
+            [['office_id', 'platform_id', 'is_primary', 'is_visible',
+                'sequence', 'created_by', 'updated_by',
+                'is_deleted', 'deleted_by', 'verlock'], 'integer'],
+            [['description'], 'string'],
+            [['created_at', 'updated_at', 'deleted_at'], 'safe'],
+            [['title'], 'string', 'max' => 100],
+            [['profile_url'], 'string', 'max' => 500],
+            [['uuid'], 'string', 'max' => 36],
+            [['office_id', 'platform_id'], 'unique',
+                'targetAttribute' => ['office_id', 'platform_id'],
+                'message' => 'The combination of Office ID and Platform ID has already been taken.'],
+            [['verlock'], 'default', 'value' => '0'],
+            [['verlock'], 'mootensai\components\OptimisticLockValidator'],
+        ];
     }
 
     /**
