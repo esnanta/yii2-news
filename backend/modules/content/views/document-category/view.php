8790d905b9c34151d6da9eb5bd46ce69a8534bb0
esnanta <ombakrinai@gmail.com>
2026-05-24 13:29:45 +0700

    update

--- backend/modules/content/views/document-category/view.php
+++ backend/modules/content/views/document-category/view.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
-/**
+/*
  * @var yii\web\View $this
  * @var common\models\DocumentCategory $model
  */
@@ -15,36 +15,33 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="document-category-view">
     <div class="card">
         <div class="card-header">
-            <?php echo Html::a(Yii::t('backend', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?php echo Html::a(Yii::t('backend', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
             <?php echo Html::a(Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
                 'class' => 'btn btn-danger',
                 'data' => [
                     'confirm' => Yii::t('backend', 'Are you sure you want to delete this item?'),
                     'method' => 'post',
                 ],
-            ]) ?>
+            ]); ?>
         </div>
         <div class="card-body">
             <?php echo DetailView::widget([
                 'model' => $model,
                 'attributes' => [
-                    'id',
-                    'office_id',
+                    [
+                        'attribute' => 'office_id',
+                        'label' => Yii::t('backend', 'Office'),
+                        'value' => static function ($model) use ($officeOptions) {
+                            return $officeOptions[$model->office_id] ?? '-';
+                        },
+                    ],
                     'title',
                     'sequence',
                     'description:ntext',
                     'created_at',
                     'updated_at',
-                    'created_by',
-                    'updated_by',
-                    'is_deleted',
-                    'deleted_at',
-                    'deleted_by',
-                    'verlock',
-                    'uuid',
-                    
                 ],
-            ]) ?>
+            ]); ?>
         </div>
     </div>
 </div>
